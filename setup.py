96435dc0072add766cdc1b0fcf18643496da2a2f
Melvie <melvie.tarr@gmail.com>
2017-07-16 15:51:43 -0700

    changed setup.py to mesh with new travis file

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     name='xi-editor',
     description='A modern editor with a backend written in Rust.',
     author='Raph Levien',
-    packages=find_packages()
+    packages=find_packages(),
+    tests_require=['pytest']
 )
